d3984919734d111363e75827081087846a279e14
José Luis Alavez Manzano <jose.alavez@softtek.com>
2022-12-02 07:40:47 -0600

    Se agrega codigo

--- stateless_app/lib/main.dart
+++ stateless_app/lib/main.dart
@@ -5,7 +5,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static const myIconSize = 40.0;
+  static const TextStyle textStyle =
+      TextStyle(color: Colors.grey, fontSize: 30.0);
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +15,34 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stateless widget'),
       ),
-      body: Container(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Card(
-                child: Column(children: [],)
-              )
-            ]),
-      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MyCard(
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.redAccent,
+                  size: myIconSize,
+                ),
+                title: Text("I love Flutter", style: textStyle),
+                    ])))
+          ]),
     );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final Widget title;
+  final Widget icon;
+
+  const MyCard({super.key, required this.title, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(children: <Widget>[this.title, this.icon]),
+    ));
   }
 }
