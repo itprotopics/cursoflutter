8d0123a52c7fcfa183405e7662bb95c00c41e613
José Luis Alavez Manzano <jose.alavez@softtek.com>
2022-12-07 13:34:24 -0600

    se agraga codigo del boton

--- statefull_app/lib/src/button.dart
+++ statefull_app/lib/src/button.dart
@@ -8,29 +8,45 @@ class MyButton extends StatefulWidget {
 }
 
 class _MyButtonState extends State<MyButton> {
+  final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+      backgroundColor: Colors.blueAccent);
+
+  String flutterText = '';
+  int index = 0;
+  List<String> collecionPalabras = ['Flutter', 'es', 'genial'];
+
+  void onPressButton() {
+    setState(() {
+      flutterText = collecionPalabras[index];
+      index = index < 2 ? index + 1 : 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Statefull widget'),
+        title: const Text('Statefull widget'),
         backgroundColor: Colors.orangeAccent,
       ),
-      body: Container(
-          child: Center(
+      body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Jose',
-              style: TextStyle(fontSize: 40),
+            Text(
+              flutterText,
+              style: const TextStyle(fontSize: 40),
             ),
+            const Padding(padding: EdgeInsets.all(10)),
             ElevatedButton(
-              child: Text('Actualizar'),
-              onPressed: () {},
-            )
+                onPressed: onPressButton,
+                style: style,
+                child: const Text('Actualizar'))
           ],
         ),
-      )),
+      ),
     );
   }
 }
